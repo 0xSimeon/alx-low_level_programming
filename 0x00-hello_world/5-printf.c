cc7b848787ab6be2e046ae7adcae77e555f0b2e3
Simeon Udoh <simeon.udoh45@gmail.com>
2023-03-17 00:30:11 +0000

    added script 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
+ * main - A program to print a line using printf
  *
- * returns: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
