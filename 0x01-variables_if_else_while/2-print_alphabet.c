d32ee8ebc27be4d4efa132110dd8275e4e8aaaec
Simeon Udoh <simeon.udoh45@gmail.com>
2023-03-17 23:03:24 +0000

    add script 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * Description: 'Prints all alphabets from a to z'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+		putchar('\n');
+	}
+	return (0);
+}
