803a8582c246e2239ab4860d8df065bc0c036d48
Simeon Udoh <simeon.udoh45@gmail.com>
2023-03-17 23:58:29 +0000

    add script 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * Description: 'prints all signle digit number from 0
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (char n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
