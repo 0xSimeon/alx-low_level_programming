f7fc5a7fcfc3e62356992c7389e10d6ce7d9ecb0
Simeon Udoh <simeon.udoh45@gmail.com>
2023-03-17 02:19:47 +0000

    added script 102

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints without printf or puts
+ *
+ * Return: 1 (Error)
+ */
+
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
