d9f0fd81aa853327aef94bb29c1379cac4a0ea12
Simeon Udoh <simeon.udoh45@gmail.com>
2023-03-18 00:17:35 +0000

    add script 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * Description: 'prints all lowecase alphabet in reverse'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
