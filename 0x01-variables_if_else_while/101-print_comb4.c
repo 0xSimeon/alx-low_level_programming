121cab13ed8b405c013c63bc548aaba92672b8c5
Simeon Udoh <simeon.udoh45@gmail.com>
2023-03-18 03:38:41 +0000

    add script 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			for (k = 2; k < 10; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
