5fe9632be7c050bb059da9b10d04e91c84f381ed
Simeon Udoh <simeon.udoh45@gmail.com>
2023-03-18 03:45:24 +0000

    add script 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,8 +21,11 @@ int main(void)
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
-					putchar(',');
-					putchar(' ');
+					if (i + j + k != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
