aeaba21ffd43c948f932180cdd3f142d6f5983c7
Simeon Udoh <simeon.udoh45@gmail.com>
2023-03-21 22:10:42 +0000

    add script 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds to integers
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Result: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return a + b;
+}
