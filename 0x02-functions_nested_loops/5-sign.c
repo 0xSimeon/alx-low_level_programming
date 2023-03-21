8f6f6297dc67d76b9c8cecb19ce6dcaf7f7807d1
Simeon Udoh <simeon.udoh45@gmail.com>
2023-03-21 21:26:13 +0000

    add script 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_sign - prints the sign of a number
- * @int n: is the integer to be checked
+ * @n: is the int to check
  * Return: 1 if n is greater than zero, 0 if n is zero. otherwise, 1
  */
 
