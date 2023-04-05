b838442cf959742c677c2404ba5058e5135b220c
Simeon Udoh <simeon.udoh45@gmail.com>
2023-04-05 04:46:17 +0000

    correct task 0 solution

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,9 +11,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; n > 0; i++)
 	{
-		s[i] = b; /* store byte b in memory block *s */
+		s[i] = b;
+		n--
 	}
 	return (s);
 }
