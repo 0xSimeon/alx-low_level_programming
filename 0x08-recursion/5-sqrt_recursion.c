e290491093208ce3c5b7453153a9a97a519d8fb2
Simeon Udoh <simeon.udoh45@gmail.com>
2023-04-07 04:40:03 +0000

    fix betty errors for task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,15 +9,15 @@
  */
 int actual_sqrt_recursion(int n, int i)
 {
-        if (i * i > n)
+	if (i * i > n)
 	{
-                return (-1);
+		return (-1);
 	}
-        if (i * i == n)
+	if (i * i == n)
 	{
-                return (i);
+		return (i);
 	}
-        return (actual_sqrt_recursion(n, i + 1));
+	return (actual_sqrt_recursion(n, i + 1));
 }
 
 
