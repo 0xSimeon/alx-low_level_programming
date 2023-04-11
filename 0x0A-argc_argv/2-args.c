5200828d6c57d23c3c1cd2b6a367854aa877ab64
Simeon Udoh <simeon.udoh45@gmail.com>
2023-04-11 01:19:48 +0000

    add solution to task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,10 +4,10 @@
  * main - prints all arguments that is passed to main
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: Always 0 (Success)		
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
-{	
+{
 	int i;
 
 	for (i = 0; i < argc; i++)
